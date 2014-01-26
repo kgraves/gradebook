66ff115493cfd7c713e11577920bc8abb6a08887
ieckert <ieckert@mail.csuchico.edu>
2014-01-26 10:04:00 -0800

    saving and loading from chrome.storage.local works

--- src/application.js
+++ src/application.js
@@ -4,6 +4,31 @@ App.Model = App.Model || {};
 (function($){
     // create global App namespace
 
-    console.log("hello");
+    /*
     var klass = new App.Model.Class();
+
+    klass.get("assignments").push([
+    { name: "assignment 1", description: "first assignment", points: 10 },
+    { name: "assignment 2", description: "second assignment", points: 20 },
+    { name: "assignment 3", description: "third assignment", points: 30 }
+    ]);
+
+    klass.get("students").push([
+    { name: "jane fonda", student_id: "123", grades: [9, 19, 29] },
+    { name: "nick cage", student_id: "456", grades: [8, 18, 28] },
+    { name: "bruce willis", student_id: "789", grades: [10, 20, 30] }
+    ]);
+
+    klass.save();
+
+    // gets everything from storage
+    chrome.storage.local.get(null, function (classes) {
+        console.log(classes);
+    });
+    */
+
+    klass = new App.Model.Class({cid: "c1"});
+    console.log(klass.fetch());
+
+
 })(jQuery);
--- src/modules/class.js
+++ src/modules/class.js
@@ -4,39 +4,39 @@ App.Collection = App.Collection || {};
 
 App.Model.Class = Backbone.Model.extend({
     defaults: {
-        name: "class 1",
-        description: "a class"
+        name: "",
+        description: "",
+        students: new App.Collection.Students(),
+        assignments: new App.Collection.Assignments()
     },
     initialize: function() {
         console.log("Class: init");
-
-        students = new App.Collection.Students();
-        assignments = new App.Collection.Assignments();
-
-        var ass1 = new App.Model.Assignment({ name: "assignment 1", description: "first assignment", points: 10 });
-        var ass2 = new App.Model.Assignment({ name: "assignment 2", description: "second assignment", points: 20 });
-        var ass3 = new App.Model.Assignment({ name: "assignment 3", description: "third assignment", points: 30 });
-        assignments.set([ass1, ass2, ass3]);
-
-        var stud1 = new App.Model.Student({ name: "jane fonda", student_id: "123", grades: [9, 19, 29] });
-        var stud2 = new App.Model.Student({ name: "nick cage", student_id: "456", grades: [8, 18, 28] });
-        var stud3 = new App.Model.Student({ name: "bruce willis", student_id: "789", grades: [10, 20, 30] });
-        students.set([stud1, stud2, stud3]);
-
-        console.log(students);
-        console.log(assignments);
     },
     fetch: function () {
-
+        var id = this.cid;
+        var klass = this;
+        chrome.storage.local.get(id, function (instance) {
+            console.log("Class: fetch");
+            klass.set(JSON.parse(instance[id]));
+        });
+        return klass;
     },
     save: function () {
-        chrome.storage.sync.set({this.id: this}, function () {
+        var klass = {};
+        klass[this.cid] = JSON.stringify(this);
+        chrome.storage.local.set(klass, function () {
             console.log("Class: save");
         });
+        return this;
     },
     destroy: function () {
-
+        var id = this.cid;
+        chrome.storage.local.remove(id, function () {
+            console.log("Class: destroy");
+        });
     },
-
 });
 
+App.Collection.Classes = Backbone.Collection.extend({
+    model: App.Model.Class
+});
